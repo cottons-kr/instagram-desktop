505f85fececde28d17dd64c54f548b717374c499
cottons-kr <devkim@hanarin.uk>
2024-01-01 05:05:24 +0900

    Refactor app startup and single instance handling

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,40 @@
 import { app } from 'electron'
 import * as path from 'path'
 import 'dotenv/config'
+import { mainWindow } from './window'
 
 export const iconPath = path.join(__dirname, '../asset/instagram.png')
 export const preloadScriptPath = path.join(__dirname, '../asset/preload.js')
 export const isDev = process.env.NODE_ENV === 'development'
 
-app.on('ready', async () => {
-  const { mainWindow, load } = await import('./window.js')
-  const { tray } = await import('./tray.js')
+const gotTheLock = app.requestSingleInstanceLock()
 
-  load(mainWindow)
-})
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
 
-app.on('new-window-for-tab', e => {
-  e.preventDefault()
-})
-
-app.setAppUserModelId('kr.cottons.instagram-desktop')
-
-app.setLoginItemSettings({
-  openAtLogin: true,
-  openAsHidden: true,
-  path: app.getPath('exe')
-})
+  app.on('ready', async () => {
+    const { mainWindow, load } = await import('./window.js')
+    const { tray } = await import('./tray.js')
+  
+    load(mainWindow)
+  })
+  
+  app.on('new-window-for-tab', e => {
+    e.preventDefault()
+  })
+  
+  app.setAppUserModelId('kr.cottons.instagram-desktop')
+  
+  app.setLoginItemSettings({
+    openAtLogin: true,
+    openAsHidden: true,
+    path: app.getPath('exe')
+  })
+}
